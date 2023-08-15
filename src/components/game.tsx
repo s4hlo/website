7154658372ae06f1b4c9220a3601ea29e69101da
RafaelM4gn <rfl.magno02@gmail.com>
2023-08-15 00:26:30 -0300

    ic 2

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,5 +1,35 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+
+const setupScene = () => {
+  const scene = new THREE.Scene();
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  return { scene, cube };
+};
+
+const setupFloor = (scene: THREE.Scene) => {
+  const floorGeometry = new THREE.PlaneGeometry(100, 100);
+  const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+  const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  floor.rotation.x = -Math.PI / 2;
+  floor.position.y = -5;
+  scene.add(floor);
+};
+
+const setupControls = (camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer, moveSpeed: number) => {
+  const controls = new PointerLockControls(camera, renderer.domElement);
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'KeyW') controls.moveForward(moveSpeed);
+    if (event.code === 'KeyS') controls.moveForward(-moveSpeed);
+    if (event.code === 'KeyA') controls.moveRight(-moveSpeed);
+    if (event.code === 'KeyD') controls.moveRight(moveSpeed);
+  });
+  return controls;
+};
 
 export const Game: React.FC = () => {
   const ref = useRef<HTMLDivElement>(null);
@@ -7,42 +37,22 @@ export const Game: React.FC = () => {
   useEffect(() => {
     if (!ref.current) return;
 
-    const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     ref.current.appendChild(renderer.domElement);
-
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
     camera.position.z = 5;
 
-    const floorGeometry = new THREE.PlaneGeometry(100, 100);
-    const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
-
-    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-
-    floor.rotation.x = -Math.PI / 2;
-    floor.position.y = -5;
-    scene.add(floor);
-
+    const { scene } = setupScene();
+    setupFloor(scene);
 
     const moveSpeed = 0.1;
+    const controls = setupControls(camera, renderer, moveSpeed);
 
-    document.addEventListener('keydown', (event) => {
-      if (event.code === 'KeyW') camera.position.z -= moveSpeed; // Forward
-      if (event.code === 'KeyS') camera.position.z += moveSpeed; // Backward
-      if (event.code === 'KeyA') camera.position.x -= moveSpeed; // Left
-      if (event.code === 'KeyD') camera.position.x += moveSpeed; // Right
+    scene.add(controls.getObject());
+    ref.current.addEventListener('click', () => {
+      controls.lock();
     });
-
-    document.addEventListener('mousemove', (event) => {
-      camera.rotation.y -= event.movementX * 0.002;
-      camera.rotation.x -= event.movementY * 0.002;
-    });
-
 
     const animate = () => {
       requestAnimationFrame(animate);
